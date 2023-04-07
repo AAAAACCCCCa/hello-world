c159065a1076c386991a47b2cc218db3c5a5a328
Administrator <1079929413@qq.com>
2023-04-07 15:06:03 +0800

    代码在每一次提交的时候，需要提供提交信息。如果是多人协同信息，需要通过信息告知他人，提交内容

---
+++ src/main/java/com/iweb/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.iweb.test;
+
+/**
+ * @author caw
+ * @date 2023/4/7 14:56
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
